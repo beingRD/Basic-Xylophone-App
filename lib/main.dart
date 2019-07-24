637fffa978007717f559c6a7df8dcf0e8ba2e922
“Rishabh <devrishabh@Rishabhs-MacBook-Pro.local>
2019-07-24 13:47:54 +0200

    vibgyor/code

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,89 @@
 import 'package:flutter/material.dart';
+import 'package:audioplayers/audio_cache.dart';
 
 void main() => runApp(XylophoneApp());
 
 class XylophoneApp extends StatelessWidget {
+  playNote(int noteNumber) {
+    AudioCache player = AudioCache();
+    player.play('note$noteNumber.wav');
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
-          child: Container(),
+          child: Center(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Expanded(
+                  child: FlatButton(
+                    color: Colors.red,
+                    onPressed: () {
+                      playNote(1);
+                    },
+                    child: Text(''),
+                  ),
+                ),
+                Expanded(
+                  child: FlatButton(
+                    color: Colors.orange,
+                    onPressed: () {
+                      playNote(2);
+                    },
+                    child: Text(''),
+                  ),
+                ),
+                Expanded(
+                  child: FlatButton(
+                    color: Colors.yellow,
+                    onPressed: () {
+                      playNote(3);
+                    },
+                    child: Text(''),
+                  ),
+                ),
+                Expanded(
+                  child: FlatButton(
+                    color: Colors.green,
+                    onPressed: () {
+                      playNote(4);
+                    },
+                    child: Text(''),
+                  ),
+                ),
+                Expanded(
+                  child: FlatButton(
+                    color: Colors.blue,
+                    onPressed: () {
+                      playNote(5);
+                    },
+                    child: Text(''),
+                  ),
+                ),
+                Expanded(
+                  child: FlatButton(
+                    color: Colors.indigo,
+                    onPressed: () {
+                      playNote(6);
+                    },
+                    child: Text(''),
+                  ),
+                ),
+                Expanded(
+                  child: FlatButton(
+                    color: Colors.purple,
+                    onPressed: () {
+                      playNote(7);
+                    },
+                    child: Text(''),
+                  ),
+                ),
+              ],
+            ),
+          ),
         ),
       ),
     );
